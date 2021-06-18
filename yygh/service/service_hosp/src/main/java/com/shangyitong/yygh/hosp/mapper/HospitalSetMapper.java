fbead5a49976edb1f24a94f3e4c9a983c5a607a8
lgl8522 <85995485+lgl8522@users.noreply.github.com>
2021-06-18 23:27:42 +0800

    Delete yygh/service directory

--- yygh/service/service_hosp/src/main/java/com/shangyitong/yygh/hosp/mapper/HospitalSetMapper.java
+++
@@ -1,9 +0,0 @@
-package com.shangyitong.yygh.hosp.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.shangyitong.yygh.model.hosp.HospitalSet;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface HospitalSetMapper  extends BaseMapper<HospitalSet> {
-}
--- yygh/service/service_hosp/src/main/java/com/shangyitong/yygh/hosp/service/HospitalSetService.java
+++
@@ -1,7 +0,0 @@
-package com.shangyitong.yygh.hosp.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.shangyitong.yygh.model.hosp.HospitalSet;
-
-public interface HospitalSetService extends IService<HospitalSet> {
-}
